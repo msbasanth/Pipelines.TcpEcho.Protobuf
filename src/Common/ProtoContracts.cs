7f442e9dabb60f40abac356c09714ce1ecb1fa61
Basanth Maipady Shreedhara <basanth.shreedhara@siemens-healthineers.com>
2019-07-09 13:26:48 +0530

    Initial submission

---
+++ src/Common/ProtoContracts.cs
@@ -0,0 +1,14 @@
+﻿using ProtoBuf;
+
+namespace Common
+{
+    class ProtoContracts
+    {
+        [ProtoContract]
+        public class Person
+        {
+            [ProtoMember(1)]
+            public string Name { get; set; }
+        }
+    }
+}
